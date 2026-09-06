9509081e915336a8f2b1b8d0d8cecccaf3e19d65
sakib-hasan-63 <mohdsakib6307@gmail.com>
2026-09-06 23:07:30 +0530

    Practice-1

---
+++ Practice-1/script.js
@@ -0,0 +1,6 @@
+// box=document.querySelectorAll(".box");
+// for(let i=0;i<box.length;i++){
+// boxes=box[i];
+// randBg="#"+Math.floor((Math.random()*1000000)+1);
+// boxes.style.backgroundColor=randBg;
+// }
